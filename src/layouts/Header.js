244a657506b89df30a02d680e6d248586d3f00f0
AlicjaLysien <alicja.lysien@gmail.com>
2021-04-17 13:05:29 +0200

    carousel

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,39 +1,34 @@
 import { React } from "react";
 import { Route, Switch } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 import header01 from "../images/header01.jpg";
 import header02 from "../images/header02.jpg";
 import header03 from "../images/header03.jpg";
 import header04 from "../images/header04.jpg";
 import header05 from "../images/header05.jpg";
 
-const Header = () => {
-    const images = [header01, header02, header03, header04, header05];
-    const index = Math.floor(Math.random() * 5);
-    const headerRandom = images[index];
+const photos = [header01, header02, header03, header04, header05]
 
-    return (
-        <>
-        <Switch>
-            <Route path="/" exact render={()=> (
-                <img src={header01} />
-            )} />
-            <Route path="/animals" render={()=> (
-                <img src={header02} />
-            )} />
-            <Route path="/prices" render={()=> (
-                <img src={header03} />
-            )} />
-            <Route path="/contact" render={()=> (
-                <img src={header04} />
-            )} />
-            <Route path="/admin" render={()=> (
-                <img src={header05} />
-            )} />
-            <Route render={()=> (
-                <img src={headerRandom} />
-            )} />
-        </Switch>
-        </>
+const Header = () => {
+
+    const slider = photos.map(photo => (
+        <Carousel.Item className="CarouselItem">
+            
+            <div className="bg" style={{backgroundImage: `url(${photo})`}}>
+                <div className="overlay"></div>
+            </div>
+        
+        <Carousel.Caption>
+          <h3>Welcome!</h3>
+          <p>Zoo</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    ))
+
+    return(
+    <Carousel>
+        {slider}
+    </Carousel>
     );
 }
 
